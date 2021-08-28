e888c5a453c4f351cf1eab8215b343c3a68e5663
George Iniatis <2329642I@student.gla.ac.uk>
2021-08-28 14:11:52 +0300

    Added Different XML Files Folder

--- DifferentXmlFiles/app/src/main/java/com/example/differentxmlfiles/MainActivity.java
+++ DifferentXmlFiles/app/src/main/java/com/example/differentxmlfiles/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.differentxmlfiles;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtHelloWorld = findViewById(R.id.txtHelloWorld);
+        txtHelloWorld.setText(getString(R.string.hello));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.settingsMenu){
+            Toast.makeText(this, "You have clicked the settings button", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.alarmMenu){
+            Toast.makeText(this, "You have clicked the alarm button", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
