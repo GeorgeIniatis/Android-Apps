69f27ce6e8586f26a2f4661c91ea569e8b044dd2
George Iniatis <2329642I@student.gla.ac.uk>
2021-08-29 15:04:26 +0300

    Added Snackbar & CardView Folder

--- Snackbar_And_CardView/app/src/main/java/com/example/snackbar_and_cardview/MainActivity.java
+++ Snackbar_And_CardView/app/src/main/java/com/example/snackbar_and_cardview/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.snackbar_and_cardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ConstraintLayout parent;
+    private Button buttonShowSnackbar;
+    private MaterialCardView cardView;
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonShowSnackbar) {
+            showSnackBar();
+        }else if(v.getId() == R.id.cardView){
+            Toast.makeText(this, "Card View Clicked", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        parent = findViewById(R.id.parent);
+        buttonShowSnackbar = findViewById(R.id.buttonShowSnackbar);
+        cardView = findViewById(R.id.cardView);
+
+        buttonShowSnackbar.setOnClickListener(this);
+        cardView.setOnClickListener(this);
     }
+
+    private void showSnackBar() {
+        Snackbar.make(parent, "This is a snackbar", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Retry", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Clicked on Retry", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setActionTextColor(Color.RED)
+                .setTextColor(Color.YELLOW)
+                .show();
+    }
+
+
 }
